d4b177255393e8d1a4314ca347afbc969953a040
Orhan DALGARA <orhandalgara@gmail.com>
2021-03-17 16:35:39 +0300

    Initial commit

---
+++ Revenuex/Revenuex/Revenuex.h
@@ -0,0 +1,18 @@
+//
+//  Revenuex.h
+//  Revenuex
+//
+//  Created by Orhan DALGARA on 17.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Revenuex.
+FOUNDATION_EXPORT double RevenuexVersionNumber;
+
+//! Project version string for Revenuex.
+FOUNDATION_EXPORT const unsigned char RevenuexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Revenuex/PublicHeader.h>
+
+
